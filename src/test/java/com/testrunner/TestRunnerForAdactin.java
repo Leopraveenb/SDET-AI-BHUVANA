1537487dbd2ce967ba6eb3df97f1e434dd0ba41e
dhanas37 <dhanasr96@gmail.com>
2026-09-22 14:39:20 +0530

    Updated testrunner files

--- src/test/java/com/testrunner/TestRunnerForAdactin.java
+++ src/test/java/com/testrunner/TestRunnerForAdactin.java
@@ -2,10 +2,12 @@ package com.testrunner;
 
 import org.junit.runner.RunWith;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+//command by Dhana
 
 	@CucumberOptions(
 	        features = "src\\test\\resources\\Feature\\AdactinHotelApp.feature",
