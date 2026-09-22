e22e09797763d7792407dd11419eec3301980dd9
dhanas37 <dhanasr96@gmail.com>
2026-09-22 15:05:46 +0530

    Revert "updated testrunner file"

--- src/test/java/com/testrunner/TestRunnerForAdactin.java
+++ src/test/java/com/testrunner/TestRunnerForAdactin.java
@@ -1,14 +1,11 @@
 package com.testrunner;
 
 import org.junit.runner.RunWith;
-//Cloned by Bhuvana
-
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-//command by Dhana
 
 	@CucumberOptions(
 	        features = "src\\test\\resources\\Feature\\AdactinHotelApp.feature",
